f516f7b7228af7c62a9ff53b18664b74f17c5f89
Solo7903 <68706446+Fave7903@users.noreply.github.com>
2022-06-21 22:33:37 +0000

    added public and procfile

--- ultraL/index.js
+++ ultraL/index.js
@@ -18,12 +18,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 .then(() => console.log("DB Connected"))
 .catch((err) => console.log(err.message))
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 app.use(expressValidator())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use('/', authRoutes)
+app.use(express.static("public"))
 app.use(function(err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({error: 'Unauthorized!'})
