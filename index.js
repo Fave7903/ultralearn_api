7d6a5b1364e6111a5e98ed05a61a24372295448b
Favour Solomon <femisolomon15@gmail.com>
2022-07-04 03:17:03 +0100

    more update

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
 const PORT = process.env.PORT || 5000
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Expose-Headers', '*')
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
-})
+});
 app.use(expressValidator())
 app.use(cors())
 app.use(bodyParser.json())
