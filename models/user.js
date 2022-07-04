e701a93f23a3c79fb1f80d8ffe9cfe1dcc26e06d
Favour Solomon <femisolomon15@gmail.com>
2022-07-05 00:04:44 +0100

    changes

--- models/user.js
+++ models/user.js
@@ -33,10 +33,10 @@ const userSchema = new mongoose.Schema({
     default: Date.now
   },
   updated: Date, 
-  // photo: {
-  //   data: Buffer,
-  //   contentType: String
-  // }
+  photo: {
+    data: Buffer,
+    contentType: String
+  }
 })
 
 userSchema.virtual('password')
