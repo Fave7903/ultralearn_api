cc36b8caa8bd5c20a7d545cf0782ef5c6cef092c
Favour Solomon <femisolomon15@gmail.com>
2022-07-05 12:02:09 +0100

    cors

--- index.js
+++ index.js
@@ -22,15 +22,14 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(expressValidator())
-app.use(cors())
 app.use('/', authRoutes)
 app.use('/', userRoutes)
 app.use('/', postRoutes)
-app.use(express.static("public"))
 app.use(function(err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({error: 'Unauthorized!'})
