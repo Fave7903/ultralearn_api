196752c3dadd041d693c41a0d8b1ad922b71c983
dutiful <dutifulgodwin2020@gmail.com>
2022-08-02 21:48:36 -0700

    'login'

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 var expressJwt = require('express-jwt')
 const User = require('../models/user')
+const bcrypt = require("bcrypt");
 
 require('dotenv').config()
 
@@ -17,32 +18,47 @@ exports.signup = async (req, res) => {
   res.status(200).json({ message: "Signup success! Please login." })
 }
 
-exports.signin = (req, res) => {
-  const {email, password} = req.body
-  User.findOne({email}, (err, user) => {
-    if (err || !user) {
-      return res.status(401).json({
-        error: "User with that email does not exist. Please signup."
-      })
+exports.login = async (req, res, next) => {
+  const user = await User.findOne({ where: {username: req.body.username }});
+  if (user) {
+    const password_valid = await bcrypt.compare(req.body.password, user.password);
+    if (password_valid){
+      token = jwt.sign({"id" : user.id, 'email' : user.email,  },process.env.JWT_SECRET)
+      res.status(200).json({ token:token });
+    } else {
+      res.status(400).json({ error: 'Password Incorrect' })
     }
-    if (!user.authenticate(password)) {
-      return res.status(401).json({
-        error: "Email and password do not match."
-      })
-    }
-    const token = jwt.sign({_id: user.id}, process.env.JWT_SECRET) 
-    res.cookie('t', token, {expire: new Date() + 9999})
-    const {_id, fullName, username, email, imgId} = user
-    return res.json({token, user: { _id, email, fullName, username, imgId} })
-  })
+  } else {
+    res.status(404).json({error: 'User does not exit' })
+  }
 }
+
+// exports.login = (req, res) => {
+//   const {email, password} = req.body
+//   User.findOne({email}, (err, user) => {
+//     if (err || !user) {
+//       return res.status(401).json({
+//         error: "User with that email does not exist. Please signup."
+//       })
+//     }
+//     if (!user.authenticate(password)) {
+//       return res.status(401).json({
+//         error: "Email and password do not match."
+//       })
+//     }
+//     const token = jwt.sign({_id: user.id}, process.env.JWT_SECRET) 
+//     res.cookie('t', token, {expire: new Date() + 9999})
+//     const {_id, fullName, username, email, imgId} = user
+//     return res.json({token, user: { _id, email, fullName, username, imgId} })
+//   })
+// }
 
 exports.signout = (req, res) => {
   res.clearCookie('t')
   return res.json({ message: "Signout success!" })
 }
 
-exports.requireSignin = expressJwt({
+exports.requirelogin = expressJwt({
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"], 
   userProperty: "auth",
